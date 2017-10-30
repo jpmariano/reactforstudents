7220205720ef3a72bfba6ac0054e79706c908d46
john <jmariano@infina.net>
2017-10-30 15:28:29 -0400

    first commit

---
+++ Ch02/02_01/finish/dist/index.js
@@ -0,0 +1,21 @@
+const { createElement } = React
+const { render } = ReactDOM
+
+const style = { 
+	backgroundColor: 'orange',
+	color: 'white',
+	fontFamily: 'verdana'
+}
+
+const title = createElement(
+	'h1',
+	{id: 'title', className: 'header', style: style},
+	'Hello World'
+)
+
+render(
+	title,
+	document.getElementById('react-container')
+)
+//npm install -g httpster 
+//httpster -d ./finish/dist -p 3000
---
+++ Ch02/02_04/finish/readme.txt
@@ -0,0 +1,3 @@
+sudo install -g babel-cli
+ sudo npm install -g babel-cli
+babel ./src/index.js --out-file ./dist/bundle.js 
